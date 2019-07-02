3eb9fb88c4e32a8c2805da36390d145e16b5aa00
洪亮 <281332545@qq.com>
2019-07-02 18:04:07 +0800

    初始化

---
+++ App/src/main/java/com/ryzx/kotlinmall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ryzx.kotlinmall.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ryzx.kotlinmall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ UserCenter/src/main/java/com/ryzx/user/Text.kt
@@ -0,0 +1,12 @@
+package com.ryzx.user
+
+/**
+ * Author： 洪亮
+ * Time： 2019-07-02 - 17:23
+ * Email：281332545@qq.com
+ * <p>
+ * 描述：
+ */
+class Text{
+    val int1 = 0
+}
